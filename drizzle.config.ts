f9de42ba3f1429304ecef1f78647e8173946ff1a
Brian Kiplagat <kiplagatbrian18@gmail.com>
2025-02-26 18:09:20 +0300

    fix 2

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,17 @@
-import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
-import env from './src/lib/env.js';
+import { defineConfig } from "drizzle-kit";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
-  schema: '.src/schema/schema.ts',
-  out: '.src/schema/migration',
-  dialect: 'mysql',
+  schema: ".src/schema/schema.ts",
+  out: ".src/schema/migration",
+  dialect: "mysql",
   dbCredentials: {
-    host: env.DB_HOST,
-    user: env.DB_USER,
-    password: env.DB_PASSWORD,
-    database: env.DB_NAME,
+    host: "localhost",
+    user: "user",
+    password: "password",
+    database: "db",
   },
   verbose: true,
   strict: true,
