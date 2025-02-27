3cb4e0c035e299b7095bb8515efdf336bf87c1c5
Brian Kiplagat <kiplagatbrian18@gmail.com>
2025-02-27 15:16:24 +0300

    fix: return

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,13 +1,13 @@
-import type { Logger as drizzleLogger } from 'drizzle-orm/logger';
-import { drizzle } from 'drizzle-orm/mysql2';
-import mysql from 'mysql2/promise';
-import * as schema from '../schema/schema.js';
-import type { userSchema } from '../schema/schema.js';
-import env from './env.js';
-import { logger } from './logger.js';
+import type { Logger as drizzleLogger } from "drizzle-orm/logger";
+import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
+import * as schema from "../schema/schema.js";
+import type { userSchema } from "../schema/schema.js";
+import env from "./env.js";
+import { logger } from "./logger.js";
 
 const DB_ERRORS = {
-  DUPLICATE_KEY: 'ER_DUP_ENTRY',
+  DUPLICATE_KEY: "ER_DUP_ENTRY",
 };
 
 export interface DatabaseError {
@@ -30,18 +30,16 @@ class DBLogger implements drizzleLogger {
   }
 }
 
-/*const connection = await mysql.createConnection({
-  host: env.DB_HOST,
-  user: env.DB_USER,
-  password: env.DB_PASSWORD,
-  database: env.DB_NAME,
-});*/
-const connection = await mysql.createPool({
+const connection = await mysql.createConnection({
   host: env.DB_HOST,
   user: env.DB_USER,
   password: env.DB_PASSWORD,
   database: env.DB_NAME,
 });
 
-const db = drizzle(connection, { schema: schema, mode: 'default', logger: new DBLogger() });
+const db = drizzle(connection, {
+  schema: schema,
+  mode: "default",
+  logger: new DBLogger(),
+});
 export { DB_ERRORS, connection, db };
