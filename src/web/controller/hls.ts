23572787eec4201482fb1198bb1444b06e8e470a
Brian Kiplagat <kiplagatbrian18@gmail.com>
2025-03-09 17:33:19 +0300

    fix: remove cron

--- src/web/controller/hls.ts
+++ src/web/controller/hls.ts
@@ -75,7 +75,7 @@ export class HLSController {
           results.push({
             videoId: video.id,
             status: "failed",
-            error: error.message,
+            error: error instanceof Error ? error.message : String(error),
           });
           continue;
         }
