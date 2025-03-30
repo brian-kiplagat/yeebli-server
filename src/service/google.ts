c9ec43b0e2673a392b4c55ff57c3d53de4d50ff3
Brian Kiplagat <kiplagatbrian18@gmail.com>
2025-03-30 14:42:29 +0300

    fix: user role

--- src/service/google.ts
+++ src/service/google.ts
@@ -64,7 +64,7 @@ export class GoogleService {
           userInfo.name || "Google User",
           userInfo.email,
           crypto.randomUUID(),
-          "user",
+          "host",
           "",
           {
             google_id: userInfo.id,
