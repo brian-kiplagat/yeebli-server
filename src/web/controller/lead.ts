b9e9fc9e2733eec9a96e6e073b09d88ebae8932e
Brian Kiplagat <kiplagatbrian18@gmail.com>
2025-04-09 14:25:34 +0300

    fix: urls

--- src/web/controller/lead.ts
+++ src/web/controller/lead.ts
@@ -291,9 +291,9 @@ export class LeadController {
       const eventLink = `${env.FRONTEND_URL}/eventpage?code=${event.id}&token=${token}&email=${validatedData.lead_form_email}`;
       const bodyText =
         event.event_type == "live_venue"
-          ? `You're invited to a live, in-person event! The venue is located at ${event.live_venue_address}. Make sure to arrive on time and enjoy the experience in person. If you have any questions or need more details, feel free to visit our website: ${eventLink}. To access the event, please use this passcode: ${token}. We look forward to seeing you there!`
+          ? `You're invited to a live, in-person event! The venue is located at ${event.live_venue_address}. Make sure to arrive on time and enjoy the experience in person. If you have any questions or need more details, feel free to visit our website: ${eventLink}. We look forward to seeing you there!`
           : event.event_type == "live_video_call"
-            ? `Get ready for a live video call event! You can join from anywhere using this link: ${event.live_video_url}. To ensure a smooth experience, we recommend joining a few minutes early. If you need more information, you can check our website here: ${eventLink}. Your access passcode is: ${token}. We can't wait to connect with you online!`
+            ? `Get ready for a live video call event! You can join from anywhere using this link: ${event.live_video_url}. To ensure a smooth experience, we recommend joining a few minutes early. If you need more information, you can check our website here: ${eventLink}.`
             : `You've booked a ticket for a virtual event! Enjoy the experience from the comfort of your own space. Simply click the link below to join: ${eventLink}. If you have any questions or need assistance, you can always visit our website. Your access passcode is: ${token}. See you there!`;
 
       sendTransactionalEmail(
