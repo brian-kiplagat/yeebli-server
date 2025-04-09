aa58f0c50c14846f4df50e1745089870e6e3cb6d
Brian Kiplagat <kiplagatbrian18@gmail.com>
2025-04-09 15:59:31 +0300

    fix: schema

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -265,7 +265,7 @@ export const leadRelations = relations(leadSchema, ({ one }) => ({
     references: [eventSchema.id],
   }),
   membership: one(membershipSchema, {
-    fields: [leadSchema.lead_status],
+    fields: [leadSchema.membership_level],
     references: [membershipSchema.id],
   }),
 }));
