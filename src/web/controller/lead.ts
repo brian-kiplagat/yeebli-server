bb878b3e4d38974adeb36008b226a611c529d454
Brian Kiplagat <kiplagatbrian18@gmail.com>
2025-04-13 16:35:14 +0300

    fix: event date

--- src/web/controller/lead.ts
+++ src/web/controller/lead.ts
@@ -156,16 +156,29 @@ export class LeadController {
           lead_id: lead[0].id,
           passcode: token,
         });
-        //send confirmation email to the lead
         const event = await this.eventService.getEvent(body.event_id);
+
         if (event) {
+          let eventDate = null;
+          if (
+            event.event_type == "live_venue" ||
+            event.event_type == "live_video_call"
+          ) {
+            const date = await this.eventService.getEventDate(
+              Number(body.event_date_id)
+            );
+            if (date) {
+              eventDate = formatDate(date.date, "YYYY DD MM HH:mm");
+            }
+          }
+          //send confirmation email to the lead
           const eventLink = `${env.FRONTEND_URL}/events/event?code=${event.id}&token=${token}&email=${body.email}`;
           const bodyText =
             event.event_type == "live_venue"
-              ? `You're invited to a live, in-person event! The venue is located at ${event.live_venue_address}. Make sure to arrive on time and enjoy the experience in person. If you have any questions or need more details, feel free to visit our website: ${eventLink}. To access the event, please use this passcode: ${token}. We look forward to seeing you there!`
+              ? `You're invited to a live, in-person event! The venue is located at ${event.live_venue_address}. Make sure to arrive on time before ${eventDate} and enjoy the experience in person. If you have any questions or need more details, feel free to visit our website: ${eventLink}. We look forward to seeing you there!`
               : event.event_type == "live_video_call"
-                ? `Get ready for a live video call event! You can join from anywhere using this link: ${event.live_video_url}. To ensure a smooth experience, we recommend joining a few minutes early. If you need more information, you can check our website here: ${eventLink}. Your access passcode is: ${token}. We can't wait to connect with you online!`
-                : `You're invited to a virtual event! Enjoy the experience from the comfort of your own space. Simply click the link below to join: ${eventLink}. If you have any questions or need assistance, you can always visit our website. Your access passcode is: ${token}. See you there!`;
+                ? `Get ready for a live video call event! You can join from anywhere using this link: ${event.live_video_url}. To ensure a smooth experience, we recommend joining a few minutes early before ${eventDate}. If you need more information, you can check our website here: ${eventLink}.`
+                : `You've booked a ticket for a virtual event! Enjoy the experience from the comfort of your own space. Simply click the link below to join: ${eventLink}. If you have any questions or need assistance, you can always visit our website. Your access passcode is: ${token}. See you there!`;
 
           sendTransactionalEmail(body.email, body.name, 1, {
             subject: `${event.event_name}`,
@@ -405,8 +418,13 @@ export class LeadController {
         });
       }
       let eventDate = null;
-      if (event.event_type == "live_venue" || event.event_type == "live_video_call") {
-        const date = await this.eventService.getEventDate(Number(validatedData.registered_date));
+      if (
+        event.event_type == "live_venue" ||
+        event.event_type == "live_video_call"
+      ) {
+        const date = await this.eventService.getEventDate(
+          Number(validatedData.registered_date)
+        );
         if (date) {
           eventDate = formatDate(date.date, "YYYY DD MM HH:mm");
         }
