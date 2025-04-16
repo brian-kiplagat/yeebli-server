26a468370ad69283878957a507788e87c7be1b72
Brian Kiplagat <kiplagatbrian18@gmail.com>
2025-04-16 16:40:36 +0300

    fix: event dates

--- src/util/string.ts
+++ src/util/string.ts
@@ -20,9 +20,9 @@ export const formatDate = (
   const d = new Date(date);
 
   // Convert UTC to local time if the date is in UTC (has 'Z' suffix)
-  if (typeof date === "string" && date.endsWith("Z")) {
+  /* if (typeof date === "string" && date.endsWith("Z")) {
     d.setMinutes(d.getMinutes() + d.getTimezoneOffset());
-  }
+  }*/
 
   const day = String(d.getDate()).padStart(2, "0");
   const month = String(d.getMonth() + 1).padStart(2, "0");
